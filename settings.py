243ca9c76f2f0262d77ef700ad1c7d3db4a15caa
Snow.Hellsing <snow.hellsing@gmail.com>
2011-11-22 20:16:10 +0800

    mf for deployment

--- settings.py
+++ settings.py
@@ -70,7 +70,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = _PROJECT_ROOT + '/webroot/s/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ PIDFILE="$PROJDIR/logs/fcgi.pid"
 
 cd $PROJDIR
 sudo ./stop.sh
-sudo ./manage.py runfcgi pidfile=$PIDFILE host=127.0.0.1 port=3033
+sudo ./manage.py runfcgi pidfile=$PIDFILE host=127.0.0.1 port=3020 daemonize=false
