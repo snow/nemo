f78f92932d09f9d511598d25659eab7873949f4a
Snow.Hellsing <snow.hellsing@gmail.com>
2011-11-22 19:52:03 +0800

    added scripts for deploying

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Replace these three settings.
+PROJDIR="/repos/workspace/nemo"
+PIDFILE="$PROJDIR/logs/fcgi.pid"
+
+cd $PROJDIR
+sudo ./stop.sh
+sudo ./manage.py runfcgi pidfile=$PIDFILE host=127.0.0.1 port=3033
