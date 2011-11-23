fab3e4c7df3afc37fe6ad146279c662bff3ea076
Snow.Hellsing <snow.hellsing@gmail.com>
2011-11-23 16:29:38 +0800

    added south

--- settings.py
+++ settings.py
@@ -133,7 +133,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'wishlist'
+    'south',
+    'wishlist',
 )
 
 # A sample logging configuration. The only tangible logging
